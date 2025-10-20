d452b3699b68b249cda3cdb3ad34534b0d11487e
lulu0003 <luja0003@kea.stud.dk>
2025-10-20 18:32:36 +0200

    javascript_virker

--- userliste.js
+++ userliste.js
@@ -1,13 +1,12 @@
 console.log("userliste loading...");
 
-const usercontainer = document.querySelector(".user_list_container");
+const donorGrid = document.querySelector(".donor-grid");
 let allData;
 let myUrl = `https://dummyjson.com/users/filter?key=gender&value=male`;
 
-getData(myUrl);
-
 function getData(userUrl) {
   console.log("get data...");
+
   fetch(userUrl).then((res) =>
     res.json().then((data) => {
       allData = data;
@@ -16,23 +15,25 @@ function getData(userUrl) {
   );
 }
 
-function showUsers(users) {
-  console.log("showUsers: ", users);
-  usercontainer.innerHTML = "";
-  users.forEach((user) => {
-    console.log("users loading...", users);
-    productcontainer.innerHTML += `<article class="card"
-     <h2>${user.firstName}</h2>;
-     <a href="produkt.html?id=${user.id}"> 
-          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
-          
-          </div>
-          <h3>${product.brandname} | ${product.subcategory}</h3>
-          <p class="info" >
-          <span>${product.price}</span>,-
-          </p>
-           <p>Læs mere:</p>
-        </a>
-      </article>`;
+getData(myUrl);
+
+function showUsers(usersObj) {
+  console.log("showUsers: ", usersObj);
+  usersObj.users.forEach((user) => {
+    console.log(user.lastName);
+
+    donorGrid.innerHTML += `<article class="donor-card">
+<a href="/donorprofil.html?id=${user.id}">
+                <img src=${user.image} alt="Donor billede" />
+                <h3> ${user.firstName} ${user.lastName}</h3>
+
+                <div>
+                <p> Øjenfarve: ${user.eyeColor}</p>
+                <p> Hårfarve: ${user.hair.color}</p>
+                <p> Højde: ${user.height}</p>
+                <p> Blodtype: ${user.bloodGroup}</p>
+                </div>
+                </a>
+            </article>`;
   });
 }
