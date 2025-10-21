7d8ffb0dbd27efc6f091579b4d94a6fd32d54926
lulu0003 <luja0003@kea.stud.dk>
2025-10-21 22:53:31 +0200

    singleview_næstenDone

--- donorprofil.js
+++ donorprofil.js
@@ -17,22 +17,35 @@ function getData() {
 
 function show(data) {
   console.log("shows data er", data);
-  userContainer.innerHTML = `<div class=".billede_user">
+  userContainer.innerHTML = ` <section class="donor_card">
+  <div class="billede_user">
   <img src="${data.image}" alt="id billede">
   </div>
-  <div class="user_info">
+  <div class="donor_info">
                 <h2 class="fuldenavn">${data.firstName} ${data.lastName}</h2>
                 <br>
                 <br>
-                <p><strong> Øjenfarve: </strong>${data.eyeColor}</p>
+                <p><span class="label">Øjenfarve:</span><span class="value">${data.eyeColor}</span></p>
                 <br>
-                <p class="info_produkt"><strong> Hårfarve: </strong>${data.hair.color}</p>
+                <p><span class="label">Hårfarve:</span><span class="value">${data.hair.color}</span></p>
                 <br>
-                <p class="info_produkt"><strong> Højde: </strong>${data.height}</p>
+                <p><span class="label">Højde:</span><span class="value">${data.height}</span></p>
                 <br>
-                <p class="info_produkt"><strong> Blodtype: </strong>${data.bloodGroup}</p>
+                <p><span class="label">Blodtype:</span><span class="value">${data.bloodGroup}</span></p>
 
+                
+                <div class="knap_række">
                 <button class="book_knap">Book tid</button>
-                <button class="like_knap">Synes godt om</button>
-                </div>  `;
+                <div class="like_knap">♡</div>
+                </div>
+                </div>
+                </section>  
+                
+                <p class="obs"> OBS: Billederne af donorer er AI-genererede replikationer og viser et vejledende udseende baseret på donorens træk. <br>
+                De er lavet for at give en idé om donorens gener og udseende, samtidig med at anonymitet og privatliv beskyttes.
+                </p>`;
 }
+
+const likeknap = document.getElementsByClassName("like_knap");
+
+likeknap.onclick = () => likeknap.classList.toggle("active");
